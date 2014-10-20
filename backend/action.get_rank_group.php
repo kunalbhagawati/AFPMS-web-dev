cd5f7832510edc98bb33b133cbb49e4cfbde1ab5
kunalbhagawati <kunal.bhagawati@yahoo.com>
2014-10-20 21:36:35 +0530

    Changing DB calls from mysqli to PDO

--- backend/action.get_rank_group.php
+++ backend/action.get_rank_group.php
@@ -1,27 +1,37 @@
 <?php
-
+/**
+ * Authors : Janani Iyer / Swati Rao
+ *
+ * Modification : Kunal Bhagawati
+ *
+ * Notes :
+ * 		Original file in mysqli, converted to PDO/mysql (Data Objects).
+ * 		Probably not good code since i'm new to PDO but still better than using mysqli
+ */
 require_once "vars/dbvars.php";
 
-$mysqlConn = mysqli_connect($host, $username, $password, $DB);
-if(mysqli_connect_errno()) {
-	throw new Exception(mysqli_connect_error());
+try{
+	$DBH = new PDO("mysql:host=$host;dbname=$DB", $username, $password);
+	
+	// $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT );	// Similar to mysql
+	$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );		// issues php warnning but continues
+
+	$ranks = array();
+	$qGetRank = $DBH->query("SELECT rank_id, rank from afpms.afpms_circular_rank_info");
+	while($row = $qGetRank->fetch(FETCH_ASSOC)) {
+		$ranks[$row['rank_id']] = $row['rank'];
+	}
+
+	$groups = array();
+	$qGetGroup = $DBH->query("SELECT group_id, group_name from afpms.afpms_circular_group_info");
+	while($row = $qGetGroup->fetch(FETCH_ASSOC)) {
+		$groups[$row['group_id']] = $row['group_name'];
+	}
+
+	echo json_encode(array('ranks'=>$ranks, 'groups'=>$groups));
+	exit;
 }
-
-$qGetRank = "SELECT rank_id, rank from afpms.afpms_circular_rank_info";
-$resGetRank = mysqli_query($mysqlConn, $qGetRank);
-
-$ranks = array();
-while($row = mysqli_fetch_assoc($resGetRank)) {
-	$ranks[$row['rank_id']] = $row['rank'];
+catch(PDOException $errObj) {
+	echo $errObj->getMessage();
+	exit;
 }
-
-$qGetGroup = "SELECT group_id, group_name from afpms.afpms_circular_group_info";
-$resGetGroup = mysqli_query($mysqlConn, $qGetGroup);
-
-$groups = array();
-while($row = mysqli_fetch_assoc($resGetGroup)) {
-	$groups[$row['group_id']] = $row['group_name'];
-}
-
-echo json_encode(array('ranks'=>$ranks, 'groups'=>$groups));
-exit;
